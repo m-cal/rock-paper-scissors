ea342ff9f1e159f625ed29acd7923593509ee3fe
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-18 15:09:48 -0400

    Do logic for getPlayerChoice()

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-function getComputerChoice(){
+function getComputerChoice() {
   let num = Math.floor(Math.random() * 100);
   console.log(num);
-  let choice = num <= 33 ? 'Rock' : 
-  num > 33 && num < 66 ? 'Paper' :
-  'Scissors';
+  let choice = num <= 33 ? 'rock' : 
+  num > 33 && num < 66 ? 'paper' :
+  'scissors';
   return choice;
 }
 
+function getPlayerChoice() {
+  prompt('What will it be? Rock, paper, or scissors?');
+}
+
 function playRound(playerSelection, computerSelection) {
+  playerSelection = getPlayerChoice().toLowerCase();
+  computerSelection = getComputerChoice();
   // plays a single round
   // returns a string that declares the winner of the round like so:
   // 'You Lose! Paper beats Rock'
