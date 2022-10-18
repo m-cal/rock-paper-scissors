c6f413087d5b4b7097ed24c6c43bd913b1cf4852
m-cal <ctrlshiftpjustdoit@gmail.com>
2022-10-18 00:19:40 -0400

    Make core function skeletons

---
+++ script.js
@@ -0,0 +1,14 @@
+function getComputerChoice(){
+  // will randomly return either rock, paper, or scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+  // plays a single round
+  // returns a string that declares the winner of the round like so:
+  // 'You Lose! Paper beats Rock'
+  // make playerSelection case-insensitive (rock, ROCK, RoCk should all work)
+}
+
+function game() {
+  
+}
